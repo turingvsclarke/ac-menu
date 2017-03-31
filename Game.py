c15039998cbf23fee049282a99804bb09edfd003
Alex Hicks <aldahick@iupui.edu>
2017-03-31 21:58:55 +0000

    Implement Game class and launcher driver

--- Game.py
+++ Game.py
@@ -1,14 +1,33 @@
-import pygame
+import json
+import os
 
-width = 32
-height = 32
+GAMES_DIR = "./games/"
 
-# Represents the game windows within the menu
-class Game(pygame.sprite.Sprite):
-  def __init__(self):
-    pygame.sprite.Sprite.__init__(self)
+class Game:
+	def __init__ (self, metadata):
+		self.title = metadata["title"]
+		self.command = metadata["command"]
 
-    self.image = pygame.Surface((width, height))
-    self.image.fill((0, 0, 0))
+	def launch (self):
+		game_dir = "{}{}/".format (GAMES_DIR, self.title)
 
-    self.rect = self.image.get_rect()
+		# Make sure we chdir back to original working directory
+		current_dir = os.getcwd ()
+		os.chdir (game_dir)
+		os.system (self.command)
+		os.chdir (current_dir)
+
+	def get_all ():
+		game_dirs = os.listdir (GAMES_DIR)
+		games = []
+
+		for game_dir in game_dirs:
+			game_dir = "{}{}/".format(GAMES_DIR, game_dir)
+			file = open ("{}metadata.json".format (game_dir), "r")
+			metadata = json.loads (file.read())
+			file.close ()
+		
+			games.append (Game (metadata))
+	
+		return games
+
---
+++ launcher_driver.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from Game import Game
+
+def main():
+	games = Game.get_all()
+	games[0].launch()
+
+if __name__ == "__main__":
+	main()
+
