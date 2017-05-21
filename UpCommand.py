116f36d58c6702c827ad8b686176058e0e7e1fd0
DannyPeck <danieljpeck93@gmail.com>
2017-05-20 21:32:57 -0400

    fixed UpCommand index out of bounds bugs

--- UpCommand.py
+++ UpCommand.py
@@ -17,12 +17,12 @@ class UpCommand (NavigationCommand):
     # calculate new selected element
     new_selected = selected - row_length
     if new_selected < 0:
-      rows = (size % row_length) - 1
+      rows = size // row_length
       new_selected = (rows * row_length) + selected
       if new_selected >= size:
         new_selected = new_selected - row_length
 
-    grid.set_selected (new_selected)
+    page.set_selected (new_selected)
 
     # select new grid element
     navigation_elements[new_selected].toggle_selected ()
