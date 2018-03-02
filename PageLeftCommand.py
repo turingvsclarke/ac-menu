31428d4a6dbb7868325607cebe546aa909973bb1
Kratos <nrupsis@yahoo.com>
2018-03-02 17:48:58 -0500

    fixed modulus by zero error for page command

--- PageLeftCommand.py
+++ PageLeftCommand.py
@@ -8,8 +8,6 @@ class PageLeftCommand (InputCommand):
     page_index = grid.get_page_index ()
     page_count = grid.get_page_count ()
 
-    if(page_count == 0):
+    if page_count != 0:
       new_page = (page_index - 1) % page_count
-    else:
-      new_page = page_index
-    grid.set_page_index (new_page)
+      grid.set_page_index (new_page)
