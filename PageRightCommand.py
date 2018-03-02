05119d6564587ae90571947c444f7d8edc06fa13
ashcl <christopher.lee.ash@gmail.com>
2018-03-02 16:45:16 -0500

    Added if logic to check for page count being 0.

--- PageRightCommand.py
+++ PageRightCommand.py
@@ -8,5 +8,8 @@ class PageRightCommand (InputCommand):
     page_index = grid.get_page_index ()
     page_count = grid.get_page_count ()
 
-    new_page = (page_index + 1) % page_count
+    if(page_count == 0):
+      new_page = (page_index + 1) % page_count
+    else:
+      new_page = page_index
     grid.set_page_index (new_page)
