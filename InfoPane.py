0445ee500958f04c76325fe7fca291fed96d7f4d
Trenton <tspice23@att.net>
2018-04-13 17:39:44 -0400

    updated Infopane to show page index

--- InfoPane.py
+++ InfoPane.py
@@ -4,7 +4,7 @@ import Colors
 
 # Represents the game windows within the menu
 class InfoPane (Sprite):
-  def __init__ (self, game, background_color, x, y, width, height):
+  def __init__ (self, game, background_color, x, y, width, height, cur_page_index, page_length_index):
     # call to super
     dimensions = (width, height)
     info_pane_image = pygame.Surface (dimensions)
@@ -12,7 +12,13 @@ class InfoPane (Sprite):
     info_pane_image.fill (background_color)
     Sprite.__init__ (self, info_pane_image, x, y, width, height)
 
+    self.cur_page = cur_page_index
+    self.page_length = page_length_index
+
     self.game = game
+
+    self.height = height
+    self.width = width
 
     self.font = pygame.font.Font (None, 32)
     self.font_color = Colors.BLACK
@@ -44,3 +50,9 @@ class InfoPane (Sprite):
     self.language_rect.x = 0
     self.language_rect.y = self.author_rect.y + self.author_rect.height
     self.image.blit (self.language, self.language_rect)
+
+    self.page_number = self.font.render ('Page: ' + str(self.cur_page) + '/' + str(self.page_length), True, self.font_color, self.font_background_color)
+    self.page_number_rect = self.title.get_rect()
+    self.page_number_rect.x = 0
+    self.page_number_rect.y = self.height - 50
+    self.image.blit (self.page_number, self.page_number_rect)
